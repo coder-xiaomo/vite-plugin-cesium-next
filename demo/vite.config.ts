a8d9479375e4960cfb1f2d6f56d89d175e8cce21
小墨 <codingzhang@126.com>
2025-06-12 07:00:06 +0800

    chore: demo 项目跑通

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -2,5 +2,11 @@ import { defineConfig } from 'vite';
 import cesium from 'vite-plugin-cesium-next';
 
 export default defineConfig({
-  plugins: [cesium()]
+  // base: './',
+  // base: '/',
+  base: '/foo/bar',
+
+  plugins: [
+    cesium(),
+  ],
 });
